2ce6eb99a174c7f7b2373f44dd27b0fd5ad0bfbd
Silver-Eun <h22sang@naver.com>
2024-05-19 03:28:53 +0900

    BoardBreak1
    create spring starter project(maven)

---
+++ boardBreak/src/main/java/com/board/boardBreak/BoardBreakApplication.java
@@ -0,0 +1,13 @@
+package com.board.boardBreak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardBreakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardBreakApplication.class, args);
+	}
+
+}
---
+++ boardBreak/src/test/java/com/board/boardBreak/BoardBreakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.board.boardBreak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardBreakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
